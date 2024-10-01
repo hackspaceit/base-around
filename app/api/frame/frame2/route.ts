97f6b6017bae04005c805a71b2bc80977b67c105
Eldar Faaiz <iniakunbisnisku@gmail.com>
2024-10-01 17:57:52 +0700

    update

---
+++ app/api/frame/frame2/route.ts
@@ -0,0 +1,65 @@
+import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
+import { NextRequest, NextResponse } from 'next/server';
+import { NEXT_PUBLIC_URL } from '../../config';
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+
+  if (!isValid) {
+    return new NextResponse('Message not valid', { status: 500 });
+  }
+
+  const text = message.input || '';
+  let state = {
+    page: 0,
+  };
+  try {
+    state = JSON.parse(decodeURIComponent(message.state?.serialized));
+  } catch (e) {
+    console.error(e);
+  }
+
+  /**
+   * Use this code to redirect to a different page
+   */
+  if (message?.button === 3) {
+    return NextResponse.redirect(
+      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
+      { status: 302 },
+    );
+  }
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        
+        {
+          action: 'link',
+          label: 'Luma Form',
+          target: 'https://lu.ma/82fvgfom?tk=cDUEj2',
+        },
+        {
+          action: 'link',
+          label: 'Devfolio',
+          target: 'https://based-sea.devfolio.co/?ref=b15c3254e7',
+        }, 
+        
+      ],
+      image: {
+        src: `${NEXT_PUBLIC_URL}/indo.png`,
+      },
+      postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
+      state: {
+        page: state?.page + 1,
+        time: new Date().toISOString(),
+      },
+    }),
+  );
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req);
+}
+
+export const dynamic = 'force-dynamic';
